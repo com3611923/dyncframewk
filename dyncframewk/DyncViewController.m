ba7fdeba2418f0314d44f8b47e650974328e6700
caobin <caobin@caobindeMacBook-Pro.local>
2016-04-05 09:30:35 +0800

    build project

---
+++ dyncframewk/DyncViewController.m
@@ -0,0 +1,39 @@
+//
+//  DyncViewController.m
+//  dyncframewk
+//
+//  Created by caobin on 16/3/23.
+//  Copyright © 2016年 caobin. All rights reserved.
+//
+
+#import "DyncViewController.h"
+
+@interface DyncViewController ()
+
+@end
+
+@implementation DyncViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor lightGrayColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ dyncframewk/Run.m
@@ -0,0 +1,18 @@
+//
+//  Run.m
+//  dyncframewk
+//
+//  Created by caobin on 16/3/22.
+//  Copyright © 2016年 caobin. All rights reserved.
+//
+
+#import "Run.h"
+
+@implementation Run
+
+-(void)testRun
+{
+    NSLog(@" run %s....",__func__);
+}
+
+@end
